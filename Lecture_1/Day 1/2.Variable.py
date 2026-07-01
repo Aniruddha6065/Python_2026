c37bb5c5c767ff855e7329516116b1c8214111d6
Aniruddha6065 <gaharwaraniruddhsingh@gmail.com>
2026-07-01 12:16:08 +0530

    day 1
    day 1

---
+++ Lecture_1/Day 1/2.Variable.py
@@ -0,0 +1,10 @@
+## Variables in python
+Name="Aniruddha"
+age=23
+price=25.23
+print(Name)
+print(age,price)
+print("My Name is :",Name)
+print("My age is:",age)
+age2=age
+print(age2)
---
+++ Lecture_1/Day 1/3.Data_Type.py
@@ -0,0 +1,6 @@
+Name="Aniruddha"
+age=23
+price=25.23
+print (type(Name))
+print (type(age))
+print(type(price))
