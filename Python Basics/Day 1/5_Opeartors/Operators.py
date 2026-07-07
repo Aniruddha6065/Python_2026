72a890b381df1ff3f620c226802b63fec6d854ab
Aniruddha6065 <gaharwaraniruddhsingh@gmail.com>
2026-07-07 12:21:37 +0530

    adding new thigs

---
+++ Python Basics/Day 1/5_Opeartors/Operators.py
@@ -0,0 +1,44 @@
+# # Arithematic Operators in Python
+
+# a=5
+# b=2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# print(a//b)
+
+# # Relational / Comparison Operators in Python
+
+# a=50
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+# Assignment Operators in Python
+
+# a=50
+# b=60
+# a=a+50
+# # or
+# a+=50
+# print(a)
+
+# Logical Operators in Python
+
+a= 45
+b= 80
+print(not True)
+print(a>b)
+val1= True
+val2= False
+print(val1 and val2)
+print(val1 or val2)
+print(not val1)
