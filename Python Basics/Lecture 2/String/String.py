d7f38ab8c2536b8f5aeb191b89a8f75a943a47ba
Aniruddha6065 <gaharwaraniruddhsingh@gmail.com>
2026-07-11 15:06:00 +0530

    note no strings

---
+++ Python Basics/Lecture 2/String/String.py
@@ -0,0 +1,27 @@
+''' String is data types that stores a sequence of characters. In Python, strings are 
+represented as sequences of Unicode characters. Strings can be created by enclosing 
+characters in either single quotes (' ') or double quotes (" "), and also in triple 
+quotes (''' ''' or """ """).'''
+
+str1='Hello ASG'
+str2="Hello ASG"
+str3='''Hello ASG'''
+print(str1 + "\n" + str2 + "\n" + str3)
+
+# 'this is ASG's tutorial' 
+
+''' In above line python get confused because here we use the sindle quotes and also we
+ have use the single apostrophe symbol so the python interpreter will think the single 
+ quotes starded and end in betwwen ASG and s wheir we use the apostrophe symboland will 
+not understanded what to do with "s tutorial" this part of string '''
+
+"this is ASG's tutorial" # this is the right way to write when we have to use apostrophe symbol
+
+# Basic Operations on Strings
+# 1. Concatenation 
+# we can concatenate two or more strings using the + operator.
+str1 = "Hello"+"ASG"
+print(str1)
+# length of string
+print(len(str1))
+
